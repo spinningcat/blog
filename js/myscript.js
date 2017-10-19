79332cfd468c0d4d91525e54ea0c42bbe9162163
spinningcat <beratn@gmail.com>
2017-10-19 11:24:47 +0300

    fix

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,19 @@
 $(function(){
+    var titles = {
+        'Operating Systems': 'Tip and tricks for GNU/Linux users',
+        'Software General': 'General software topics like software design pattern, naming convention, static type, dynamic type? etc.',
+        'Web General': 'The area to discuss some spesific matters ',
+        'FrontEnd': 'CSS, Javascript, jQuery and/or some frameworks',
+        'Backend': 'Examples',
+        'Editorial': 'Possible options for editting',
+        'Useful Node Packages': 'Short Examples with Handy Node Packages',
+        'Useful JavaScript Libraries' : 'Some External Libraries You want to take a look at',
+        'New Tech': 'Developments that need to be discovered',
+        'Mobile': 'Ionic, React Native brief mobile development',
+        'Unity': 'Unity development',
+        'Tool For Deployment Software' : 'Git, SVN ',
+        'Projects': 'Partial telling about project with some code samples'
+      };
 var obj = ['Web Dev Resources','Start working with canvas','Git Extras','Git Annex',
 'Canvas project 2d web based game','Working swith flexbox - Basic'
 , 'Fux and Grid Design','A real world example with flexbox', 'Markdown Syntax', 
@@ -16,6 +31,9 @@ var obj = ['Web Dev Resources','Start working with canvas','Git Extras','Git Ann
 'Linux File Permission',
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of b2122a0... finally come back
 'Naming Convention',
 'Type of Language',
 'Software Design Pattern - Singeleton Pattern',
@@ -37,12 +55,15 @@ htmlPart.innerHTML = htmlTemplate;
 
 
 //var dt = categoryContainer.querySelector('dl');
+<<<<<<< HEAD
 =======
 'Software Design Pattern' ];
 >>>>>>> parent of 66b4c53... categorization
 =======
 'Software Design Pattern' ];
 >>>>>>> parent of 66b4c53... categorization
+=======
+>>>>>>> parent of b2122a0... finally come back
 $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
 $( ".expandnarrow" ).each(function( index, element ) {
    console.log(obj[index]);
@@ -92,5 +113,61 @@ document.onclick = function(e) {
         }
     });
     });
+   
+    
+
+    
+    function constructPartialMarkup() {
+        
+      var parser = new DOMParser;
+      for (var items in titles) {
+                 template = parser.parseFromString(htmlTemplate, "text/html");
+           
+           template.querySelector('dt').setAttribute('class', S(items).strip(' ', '_', '-').s);
+           template.querySelector('dt').innerHTML = items;
+           template.querySelector('dt').addEventListener('click',
+           appendArticlesToDiv);
+           template.querySelector('dd').innerHTML = titles[items];
+           categoryContainer.appendChild(template.firstChild);
+      }
+    }
+    constructPartialMarkup();
+    var subContainer = document.querySelectorAll('.subContainer');
+   
+    var titleContainer = document.querySelector('.titleContainer');
+    var titleSub = document.querySelectorAll('.titleContainer .subContainer');
+    var arrowContainer = document.querySelector('.arrowContainer');
+    
+    var dt = document.querySelectorAll('.categoryContainer dt');
+    var classAttr;
+    function appendArticlesToDiv(){
+        
+        categoryContainer.classList.add('hiding');
+        titleContainer.classList.remove('hiding');
+        classAttr = this.getAttribute('class');
+     
+       
+      
+        subContainer.forEach(function(element, index){
+           
+            if(classAttr === element.getAttribute('data')){
+                titleContainer.appendChild(element);
+             }
+        });
+        $(titleContainer).find(subContainer).removeClass('hiding');
+        arrowContainer.classList.remove('hiding');
+    
+        
+        
+       
+       
+    }
+    arrowContainer.addEventListener('click', function(){
+        arrowContainer.classList.add('hiding');
+        titleContainer.classList.add('hiding');
+        categoryContainer.classList.remove('hiding');
+        titleContainer.innerHTML = '';
+    });
+   
 });
 
